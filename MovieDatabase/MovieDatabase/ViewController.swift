f3e88e825a47544bd312cb8cdb3c83f222a8cc97
Richard Leh <richard@guarulhos.org>
2019-03-05 13:26:26 -0300

    Add project

---
+++ MovieDatabase/MovieDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDatabase
+//
+//  Created by Richard Leh on 05/03/19.
+//  Copyright © 2019 Richard Leh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
